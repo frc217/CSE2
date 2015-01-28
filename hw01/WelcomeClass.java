80f119cf64c5c0ba6e941804942ab5fcba06f8b7
frc217 <frc217@lehigh.edu>
2015-01-28 00:45:14 +0000

    created and completed WelcomeClass assignment

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////
+//  Frederick Coleman
+//  CSE 002 Section 111
+//  January 27 2015
+//  HW 01
+//  Welcome Class Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+    //  add main method
+    public static void main(String[] args) {
+    
+    //  print the statement with the box around it
+    System.out.println("    ----------- ");
+    System.out.println("    | WELCOME | ");
+    System.out.println("    ----------- ");
+    System.out.println("  ^  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println("I recently visited Ghana this winter ");
+    System.out.println("break. I am planning on returning to ");
+    System.out.println("the country to do research through ");
+    System.out.println("Lehigh University.");
+    
+    }
+}
